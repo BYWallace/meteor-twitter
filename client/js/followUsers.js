5ac99ffa109bef8477341cd89c1c635b691a96eb
Brett Wallace <bwallace90@gmail.com>
2015-10-10 16:00:40 -0400

    Fix user search bug

--- client/js/followUsers.js
+++ client/js/followUsers.js
@@ -16,7 +16,7 @@ Template.followUsers.helpers({
 
 Template.followUsers.events({
   'submit form': function(event) {
-    var searchUser = event.target.searchUser.value();
+    var searchUser = event.target.searchUser.value;
     var foundUsers = Meteor.call('findUser', searchUser, function(err, res) {
       if (res) {
         Session.set('foundUser', res);
